69c16843f72606017f7de27e09a6360f0ca8ce48
Jie <linsijia628@gmail.com>
2025-02-08 19:59:33 +0800

    chore: github actions

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,42 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+
+/// Official API methods
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ApiMethod {
+    #[serde(rename = "getMe")]
+    GetMe,
+    #[serde(rename = "createInvoice")]
+    CreateInvoice,
+    #[serde(rename = "deleteInvoice")]
+    DeleteInvoice,
+    #[serde(rename = "createCheck")]
+    CreateCheck,
+    #[serde(rename = "deleteCheck")]
+    DeleteCheck,
+    #[serde(rename = "transfer")]
+    Transfer,
+    #[serde(rename = "getInvoices")]
+    GetInvoices,
+    #[serde(rename = "getChecks")]
+    GetChecks,
+    #[serde(rename = "getTransfers")]
+    GetTransfers,
+    #[serde(rename = "getBalance")]
+    GetBalance,
+    #[serde(rename = "getExchangeRates")]
+    GetExchangeRates,
+    #[serde(rename = "getCurrencies")]
+    GetCurrencies,
+    #[serde(rename = "getStats")]
+    GetStats,
+}
+
+impl std::fmt::Display for ApiMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Currency {
@@ -84,4 +122,40 @@ pub enum PayButtonName {
     OpenChannel,
     OpenBot,
     Callback,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AppStats {
+    /// Total volume of paid invoices in USD.
+    pub volume: u64,
+    /// Conversion of all created invoices.
+    pub conversion: u64,
+    /// The unique number of users who have paid the invoice.
+    pub unique_user_count: u64,
+    /// Total created invoice count.
+    pub created_invoice_count: u64,
+    /// Total paid invoice count.
+    pub paid_invoice_count: u64,
+    /// The date on which the statistics calculation was started in ISO 8601 format.
+    pub start_at: DateTime<Utc>,
+    /// The date on which the statistics calculation was ended in ISO 8601 format.
+    pub end_at: DateTime<Utc>,
+}
+
+// TODO: Add validation
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GetStatsParams {
+    /// Optional. Date from which start calculating statistics in ISO 8601 format. Defaults is current date minus 24 hours.
+    pub start_at: Option<DateTime<Utc>>,
+    /// Optional. Date to which end calculating statistics in ISO 8601 format. Defaults is current date.
+    pub end_at: Option<DateTime<Utc>>,
+}
+// TODO
+impl std::default::Default for GetStatsParams {
+    fn default() -> Self {
+        Self {
+            start_at: Some(Utc::now() - chrono::Duration::hours(24)),
+            end_at: Some(Utc::now()),
+        }
+    }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,13 +1,19 @@
-use chrono::DateTime;
-
 use crate::{
     balance::Balance,
+    check::{Check, CreateCheckParams, GetChecksParams},
     exchange::ExchangeRate,
-    invoice::{CreateInvoiceParams, GetInvoicesParams, Invoice, InvoiceStatus},
-    model::CryptoCurrencyCode,
+    invoice::{CreateInvoiceParams, GetInvoicesParams, Invoice},
+    model::{AppStats, CryptoCurrencyCode, Currency, GetMeResponse, GetStatsParams},
     transfer::{Transfer, TransferParams},
     CryptoBotError,
 };
+
+#[async_trait::async_trait]
+pub trait BaseClient {
+    async fn get_me(&self) -> Result<GetMeResponse, CryptoBotError>;
+    async fn get_currencies(&self) -> Result<Vec<Currency>, CryptoBotError>;
+    async fn get_stats(&self, params: &GetStatsParams) -> Result<AppStats, CryptoBotError>;
+}
 
 #[async_trait::async_trait]
 pub trait BalanceClient {
@@ -15,7 +21,11 @@ pub trait BalanceClient {
 }
 
 #[async_trait::async_trait]
-pub trait CheckClient {}
+pub trait CheckClient {
+    async fn create_check(&self, params: &CreateCheckParams) -> Result<Check, CryptoBotError>;
+    async fn delete_check(&self, check_id: i64) -> Result<bool, CryptoBotError>;
+    async fn get_checks(&self, params: &GetChecksParams) -> Result<Vec<Check>, CryptoBotError>;
+}
 
 #[async_trait::async_trait]
 pub trait ExchangeClient {
@@ -35,35 +45,31 @@ pub trait TransferClient {
 
 #[async_trait::async_trait]
 pub trait InvoiceClient {
-    /// Creates a new invoice
     async fn create_invoice(&self, params: &CreateInvoiceParams)
         -> Result<Invoice, CryptoBotError>;
-
-    /// Deletes an invoice
     async fn delete_invoice(&self, invoice_id: i64) -> Result<bool, CryptoBotError>;
-
-    /// Gets invoices by specified parameters
     async fn get_invoices(
         &self,
         params: &GetInvoicesParams,
     ) -> Result<Vec<Invoice>, CryptoBotError>;
 
-    /// Gets information about a specific invoice
-    async fn get_invoice(&self, invoice_id: i64) -> Result<Invoice, CryptoBotError>;
+    // ! These extra methods could be added to the struct, but they are not part of the official API
+    // /// Gets information about a specific invoice
+    // async fn get_invoice(&self, invoice_id: i64) -> Result<Invoice, CryptoBotError>;
 
-    /// Confirms paid status of an invoice
-    async fn confirm_paid_invoice(
-        &self,
-        invoice_id: i64,
-        paid_at: DateTime<chrono::Utc>,
-    ) -> Result<Invoice, CryptoBotError>;
+    // /// Confirms paid status of an invoice
+    // async fn confirm_paid_invoice(
+    //     &self,
+    //     invoice_id: i64,
+    //     paid_at: DateTime<chrono::Utc>,
+    // ) -> Result<Invoice, CryptoBotError>;
 
-    /// Checks if an invoice is paid
-    async fn is_invoice_paid(&self, invoice_id: i64) -> Result<bool, CryptoBotError>;
+    // /// Checks if an invoice is paid
+    // async fn is_invoice_paid(&self, invoice_id: i64) -> Result<bool, CryptoBotError>;
 
-    /// Checks if an invoice is expired
-    async fn is_invoice_expired(&self, invoice_id: i64) -> Result<bool, CryptoBotError>;
+    // /// Checks if an invoice is expired
+    // async fn is_invoice_expired(&self, invoice_id: i64) -> Result<bool, CryptoBotError>;
 
-    /// Gets the status of an invoice
-    async fn get_invoice_status(&self, invoice_id: i64) -> Result<InvoiceStatus, CryptoBotError>;
+    // /// Gets the status of an invoice
+    // async fn get_invoice_status(&self, invoice_id: i64) -> Result<InvoiceStatus, CryptoBotError>;
 }
